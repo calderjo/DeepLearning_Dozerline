0a995ca177b509a51e91431936ce56bbc7d34a13
calderjo <calderjo@sonoma.edu>
2021-10-31 04:11:06 -0700

    working on tensor flow version

--- createDataSplit.py
+++ createDataSplit.py
@@ -1,54 +1,48 @@
 import os
 import numpy as np
-from sklearn import model_selection
-from sklearn import preprocessing
-from PIL import Image, ImageOps
+import tensorflow as tf
+from tensorflow import data
+
+"""
+Cool thing is that tensor flow dataset have a map function where an operation passed can be performed
+across the entire dataset
+"""
 
 
-def file_to_numpy(directory, is_mask):
+def parse_image(img_path: str) -> dict:
+    """Load an image and its annotation (mask) and returning
+    a dictionary.
+    :param
+    img_path : str - Image (not the mask) location.
 
-    # this list will hold numpy array of our image chips
-    all_images = []
-    image_names = os.listdir(directory)
-    image_names = sorted(image_names)
+    :returns
+    dict: Dictionary mapping an image and its annotation.
+    """
 
-    for imageChip in image_names:
+    image = tf.io.read_file(img_path)  # reads in an image
+    image = tf.image.decode_png(image, channels=3)
+    image = tf.image.convert_image_dtype(image, tf.uint8)
 
-        im = Image.open(directory + imageChip)  # load image
-        gray_img = ImageOps.grayscale(im)       # grayscale image
+    mask_path = tf.strings.regex_replace(img_path, "images", "labels")  # these two lines find the associated mask
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=1)
+    mask = tf.where(mask == 255, np.dtype('uint8').type(1), mask)  # remaps 255 - is dozer to 1
 
-        if is_mask:
-            im_resize = im.resize((388, 388))
-            arr = np.array(im_resize)
-
-        else:  # is an image chips
-            arr = np.asarray(gray_img)
-            arr = arr / 255
-            arr = arr.reshape([572, 572, 1])
-
-        all_images.append(arr)
-
-    img_data_np = np.array(all_images)
-    return img_data_np
+    return {'image': image, 'segmentation_mask': mask}
 
 
-def create_data_split(image_directory, label_directory, train_ratio, val_ratio, test_ratio, seed):
+def load_image_dataset(path_dataset, seed):
+    """
+    :param
+    path_dataset: takes in the path of the image dataset assume that directory is in the form
+        dataset:
+            images:
+            labels:
+    seed: select seed for shuffle
+    :return: return tensor flow dataset
+    """
 
-    image_data = file_to_numpy(image_directory, False)
-    mask_data = file_to_numpy(label_directory, True)
+    dataset = tf.data.Dataset.list_files(path_dataset + "images/*.png", shuffle=True, seed=seed)
+    dataset.map(parse_image)  # after mapping parse_image, dataset now holds both image and mask
 
-    # first split creates a train set with x percent of the data
-    test_size = 1 - train_ratio
-
-    train_data, test_val_data, train_label, test_val_label = \
-        model_selection.train_test_split(image_data, mask_data, test_size=test_size, random_state=seed, shuffle=True)
-
-    # first split creates a val set with y percent of data and a test set with z percent of the data
-    test_size = test_ratio / (test_ratio + val_ratio)
-
-    val_data, test_data, val_label, test_label = \
-        model_selection.train_test_split(test_val_data, test_val_label, test_size=test_size, random_state=seed,
-                                         shuffle=True)
-
-    # Returning the data split
-    return train_data, train_label, val_data, val_label, test_data, test_label
+    dataset.shuffle(buffer_size=dataset)
